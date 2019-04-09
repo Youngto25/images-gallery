1c3f33ec37272197b734a7e75ff0913c2ecbce0d
Blake <2979337714@qq.com>
2019-04-09 18:05:14 +0800

    点击图片，简介也会随之变化

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 function showPic(whichPic){
     var source = whichPic.getAttribute('href')
+    var text = whichPic.getAttribute('title')
     var placeholder = document.getElementById('placeholder')
     placeholder.setAttribute('src',source)
+    var description = document.getElementById('description')
+    description.childNodes[0].nodeValue = text
 }
+
+//显示body元素的子元素的总个数
+function countBodyChildren(){
+    var body_element = document.getElementsByTagName('body')
+    console.log(body_element[0])
+    console.log(body_element[0].childNodes)
+}
+
+//在页面加载时调用countBodyChildren函数
+window.onload = countBodyChildren()
